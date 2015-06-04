3ceb6c23a99b94a6947bc8b93274fdafd445090c
fjodorov <a.s.fjodorov@mail.ru>
2015-06-04 23:15:16 +0600

    Added class GameHelper in Glava5Ex1

---
+++ src/Glava5Ex1/GameHelper.java
@@ -0,0 +1,17 @@
+package Glava5Ex1;
+import java.io.*;
+public class GameHelper {
+    public String getUserInput(String prompt){
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader (
+            new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) return null;
+        } catch (IOException e){
+            System.out.println("IOException: " + e);
+        }//catch
+        return inputLine;
+    }//getUserInput
+}//class
