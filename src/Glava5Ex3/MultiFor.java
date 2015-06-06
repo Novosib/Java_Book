038a51568d89b67d4a5f0ef623fa49ffc8e41ff7
fjodorov <a.s.fjodorov@mail.ru>
2015-06-06 09:49:59 +0600

    Added class MultiFor in Glava5Ex3

---
+++ src/Glava5Ex3/MultiFor.java
@@ -0,0 +1,13 @@
+package Glava5Ex3;
+class MultiFor {
+    public static void main(String[] args) {
+        for (int x = 0; x < 4; x++){
+            for (int y = 4; y > 2; y--){
+                System.out.println(x + " " + y);
+            }
+            if (x == 1){
+                x++;
+            }
+        }//for 0 <= x <= 3
+    }//main
+}//class
