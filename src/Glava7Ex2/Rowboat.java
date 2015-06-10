dc51d710cf088950c7f2934491919a4d57db714f
fjodorov <a.s.fjodorov@mail.ru>
2015-06-10 20:40:09 +0600

    Added Glava7Ex2 - Nasledovanie, Polimorfizm

---
+++ src/Glava7Ex2/Rowboat.java
@@ -0,0 +1,6 @@
+package Glava7Ex2;
+public class Rowboat extends Boat{
+    public void rowTheBoat(){
+        System.out.print("Давай, давай, давай!!!");
+    }
+}//class
---
+++ src/Glava7Ex2/Sailboat.java
@@ -0,0 +1,6 @@
+package Glava7Ex2;
+public class Sailboat extends Boat{
+    public void move(){
+        System.out.print("Поднять паруса!");
+    }
+}//class
