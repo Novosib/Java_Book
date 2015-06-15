420792df9f885bb2999208bdb0a9735db879dd02
fjodorov <a.s.fjodorov@mail.ru>
2015-06-15 08:14:00 +0600

    Added Glava10Ex3 - Тема: Форматирование строки и даты

---
+++ src/Glava10Ex3/TestFormats.java
@@ -0,0 +1,10 @@
+package Glava10Ex3;
+public class TestFormats {
+    public static void main(String[] args) {
+        String s1 = String.format("%,d; %,.3f", 1000, 1000000.12345678);
+        System.out.println("Результат форматирования чисел = " + s1);
+        java.util.Date today= new java.util.Date();
+        String s2 = String.format("%tA, %<tB %<td", today);
+        System.out.println("Новая дата = " + s2);
+    }//main
+}//class
