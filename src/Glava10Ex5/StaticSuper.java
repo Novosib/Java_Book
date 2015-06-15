2ded269fcbf57611ebb7572552cc2bd7ee9dd97b
fjodorov <a.s.fjodorov@mail.ru>
2015-06-15 22:30:30 +0600

    Added Glava10Ex5 - Тестовое задание № 1

---
+++ src/Glava10Ex5/StaticSuper.java
@@ -0,0 +1,10 @@
+package Glava10Ex5;
+public class StaticSuper {
+    static {
+        System.out.println("Родительский статический блок");
+    }
+    StaticSuper(){
+        System.out.println(
+            "Родительский конструктор");
+    }
+}//class
---
+++ src/Glava10Ex5/StaticTest.java
@@ -0,0 +1,15 @@
+package Glava10Ex5;
+public class StaticTest extends StaticSuper{
+    static int rand = (int) (Math.random() * 6); 
+    static{
+        System.out.println("Статический блок дочернего класса " + rand);
+    }
+    StaticTest() {
+        System.out.println("Конструктор дочернего класса");
+    }
+    public static void main(String[] args) {
+        System.out.println("Внутри main");
+        StaticTest st = new StaticTest();
+    }//main
+    
+}//class
