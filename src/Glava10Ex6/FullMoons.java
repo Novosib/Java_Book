8bc73ffa6184df8b2d5873f933a9af6020ea0f37
fjodorov <a.s.fjodorov@mail.ru>
2015-06-16 19:42:33 +0600

    Added Glava10Ex6 Тестовое задание №2

---
+++ src/Glava10Ex6/FullMoons.java
@@ -0,0 +1,18 @@
+package Glava10Ex6;
+
+import java.util.Calendar;
+import static java.lang.System.out;
+
+public class FullMoons {
+    static int DAY_IM = 1000 * 60 * 60 * 24;
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        c.set(2004, 0, 7, 15, 40);
+        long day = c.getTimeInMillis();
+        for (int i = 0; i < 12; i++) {
+            day += DAY_IM * 29.52;
+            c.setTimeInMillis(day);
+            out.println(String.format("Полнолуние было в %tc", c));
+        }//for
+    }//main
+}//class
